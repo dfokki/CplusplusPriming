d8876339b945fc3dd6894eb68605d0ce08448df5
dfokki <sonata_jjk@hotmail.com>
2017-10-22 21:37:27 +0300

    no message

--- CplusplusPriming/StringsVectorsArraysTests/Source.cpp
+++ CplusplusPriming/StringsVectorsArraysTests/Source.cpp
@@ -2,8 +2,22 @@
 #include <iostream>
 #include<string>
 #include <vector>
+
+std::string str;
+void foo();
+void IStream();
+
 void main(void) {
-	std::string str;
+
+	int i[] = { 2,3,4,5 };
+	auto ip(i);
+
+	int *ip2 = ip + 1;
+
+	int *ip3 = ip + 2;
+
+	std::cout << *ip <<"  "<< *ip2 << "  "<< ip3 << std::endl;
+
 	while (getline(std::cin, str))
 	{
 		int i = 0;
@@ -29,7 +43,9 @@ void main(void) {
 	std::cout << "size of svec2 = " << svec2.size() << std::endl;
 	std::cout << "size of svec3 = " << svec3.size() << std::endl;
 
-	while (std::cin >> str)
+
+	// push only numbers to svec3
+	while (std::cin >> str && str != "next")
 	{
 		std::string temp;
 		
@@ -60,10 +76,56 @@ void main(void) {
 		std::cout << "size of svec = " << svec.size() << std::endl;
 		std::cout << "size of svec2 = " << svec2.size() << std::endl;
 		std::cout << "size of svec3 = " << svec3.size() << std::endl;
+
+		std::cout << "write next to continue "<<std::endl;
+
+
 	}
 
+	foo(); 
 
 
 	exit(EXIT_SUCCESS);
 
+}
+
+void IStream(void* function)
+{
+	while (std::cin >> str && str != "next")
+	{
+		function;
+		std::cout << "write next to continue " << std::endl;
+
+
+	}
+}
+
+void foo()
+{
+	IStream();
+
+	while (getline(std::cin, str))
+	{
+		int i = 0;
+		for (auto c : str)
+		{
+			if (isdigit(c))
+				exit(EXIT_FAILURE);
+
+			c = toupper(c);
+			std::cout << c << std::endl;
+			std::cout << str;
+		}
+		break;
+	}
+	std::vector<std::string> svec(2);
+	std::vector<std::string> svec2{ 10,"10" };
+	std::vector<std::string> svec3;
+
+	for (auto c : svec)
+		std::cout << c << std::endl;
+
+	std::cout << "size of svec = " << svec.size() << std::endl;
+	std::cout << "size of svec2 = " << svec2.size() << std::endl;
+	std::cout << "size of svec3 = " << svec3.size() << std::endl;
 }
